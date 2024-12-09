20274359f70781305b8724962ff8972db7a83b1a
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2024-12-09 08:05:45 -0500

    Create launcher.py

---
+++ launcher.py
@@ -0,0 +1,23 @@
+import xbmc
+import xbmcgui
+import xml.etree.ElementTree as ET
+import sys
+
+def main():
+    dialog = xbmcgui.Dialog()
+    feeds = [
+        ("Source #1", "RunScript(Q:\\scripts\\Alexandria\\source1.py)"),
+        ("Source #2", "RunScript(Q:\\scripts\\Alexandria\\source2.py)"),
+        ("Source #2", "RunScript(Q:\\scripts\\Alexandria\\source3.py)"),
+    ]
+    
+    feed_list = [name for name, _ in feeds]
+    selected = dialog.select(u"Alexandria - Internet Archive Downloader", feed_list)
+    
+    if selected >= 0:
+        name, url = feeds[selected]
+        if "RunScript" in url:
+            xbmc.executebuiltin(url)
+
+if __name__ == '__main__':
+    main()
